325fb9ea2579f5fea5026fed355d4f3d811325af
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-10 19:29:22 +0100

    wip

--- data/migrations/20201210192425_first_migration.js
+++ data/migrations/20201210192425_first_migration.js
@@ -1,12 +1,14 @@
 // zoos, animals, species, zoo_animals
-exports.up = function(knex) {
-  return knex.schema.createTable('zoos', table => {
-    table.increments('zoo_id')
-    table.string('zoo_name', 128).notNullable()
-    table.string('address', 128).notNullable()
-  })
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('zoos', table => {
+      table.increments('zoo_id')
+      table.string('zoo_name', 128).notNullable()
+      table.string('address', 128).notNullable()
+    })
+    .createTable('animals')
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTableIfExists('zoos')
 };
